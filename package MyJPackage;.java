1a45c1db500b9011e0a72d4f74875e06fa5bd4b2
HarshAryan001 <aryanharsh93@gmail.com>
2022-09-12 11:48:59 +0530

    file added

---
+++ package MyJPackage;.java
@@ -0,0 +1,34 @@
+package MyJPackage;
+
+
+public class JavaClass {
+
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter your age:");
+//		int b = sc.nextInt();
+//		System.out.println("Age is");
+//		System.out.println(b);
+		
+//		int num1=12;
+//		int num2=18;
+//		double result = (double)num1/(double)num2;
+//		System.out.println(result);
+		
+//		String Str1 = "30";
+//		String Str2 = "50";
+		
+//		System.out.println(Str1+Str2);
+		
+		String s1= "Aryan is bad boy";
+		String s2= "No";
+		System.out.println(s1.length());
+		System.out.println(s1.indexOf("is"));
+		System.out.println(s1.toUpperCase());
+		System.out.println(s1.concat(s2));
+		
+		}
+
+	}
+
+
